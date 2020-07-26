856d30ecfc14bdb5e39047b30f3a8f5baaa7eeb8
George <gtsianakas94@gmail.com>
2020-07-26 16:22:58 +0300

    SecurityJWT Commit
    Adding Spring Security Configuration and JWT Authentication

--- server/src/main/java/com/privateschool/server/service/UserDetailsServiceImpl.java
+++ server/src/main/java/com/privateschool/server/service/UserDetailsServiceImpl.java
@@ -1,13 +1,37 @@
 package com.privateschool.server.service;
 
+import com.privateschool.server.model.User;
+import com.privateschool.server.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElse(null);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole().name()));
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
     }
+
 }
